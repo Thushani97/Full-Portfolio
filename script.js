2b6503cbe577e8e437bd5e9929cd547e533556d3
Thushani Chandrasekara <9thushani7@gmail.com>
2020-11-10 12:35:38 +0530

    edit4

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass('sticky');
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass('show');
+        }
+        else{
+            $('.scroll-up-btn').removeClass('show');
+        }
     });
 
+    //  slide up scroll
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
     // toggle menu/ navbar script
 
     $('.menu-btn').click(function(){
